a5f6a4bf478e04c1632daa0a11df67722f037011
jat <jattdb@gmail.com>
2017-05-25 12:17:48 -0400

    Adding ability to construct from predefined ASM as well as image files

--- tools/create_asm.py
+++ tools/create_asm.py
@@ -3,12 +3,12 @@ import argparse
 import time
 
 
-def generate_asm(foreground,
-                 background,
-                 beat_data,
-                 note_data,
-                 out,
-                 template):
+def generate_asm_from_images(foreground,
+                             background,
+                             beat_data,
+                             note_data,
+                             out,
+                             template):
     foreground_asm = image_converter.gen_asm_from_image_file(foreground)
     background_asm = image_converter.gen_asm_from_image_file(background, prefix='Alt')
     with open(beat_data) as f:
@@ -16,6 +16,36 @@ def generate_asm(foreground,
 
     with open(note_data) as f:
         note_asm = f.read()
+
+    beat_range_lengths = count_segment_lengths(beat_asm)
+    note_range_lengths = count_segment_lengths(note_asm)
+
+    with open(template) as f:
+        asm_text = f.read()
+    asm_text = asm_text.format(ForegroundImage=foreground_asm,
+                               BackgroundImage=background_asm,
+                               BeatData=beat_asm,
+                               NoteData=note_asm,
+                               BeatDataALen=beat_range_lengths['A'],
+                               BeatDataBLen=beat_range_lengths['B'],
+                               BeatDataCLen=beat_range_lengths['C'],
+                               NoteDataALen=note_range_lengths['A'],
+                               NoteDataBLen=note_range_lengths['B'],
+                               NoteDataCLen=note_range_lengths['C']
+                               )
+
+    with open(out, 'w') as f:
+        f.write(asm_text)
+
+    return asm_text
+
+
+def generate_asm(foreground_asm,
+                 background_asm,
+                 beat_asm,
+                 note_asm,
+                 out,
+                 template):
 
     beat_range_lengths = count_segment_lengths(beat_asm)
     note_range_lengths = count_segment_lengths(note_asm)
@@ -54,10 +84,12 @@ def count_segment_lengths(asm_text):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('foreground_image', type=str, nargs=1, help='Path to foreground image file')
-    parser.add_argument('background_image', type=str, nargs=1, help='Path to background image file')
-    parser.add_argument('--beat_file', type=str, nargs=1, help='Path to file containing beat data, default dehuman_beat_data.asm', default='dehuman_beat_data.asm')
-    parser.add_argument('--note_file', type=str, nargs=1,help='Path to file containing note data, default dehuman_note_data.asm', default='dehuman_note_data.asm')
+    parser.add_argument('--foreground_image', type=str, help='Path to foreground image file. If specified, overrides fg_file data', default=None)
+    parser.add_argument('--background_image', type=str, help='Path to background image file. If specified, overrides bg_file data', default=None)
+    parser.add_argument('--fg_file', type=str, help='Path to file containing foreground ASM data, default dehuman_foreground_data.asm', default='dehuman_foreground_data.asm')
+    parser.add_argument('--bg_file', type=str, help='Path to file containing background ASM data, default dehuman_background_data.asm', default='dehuman_background_data.asm')
+    parser.add_argument('--beat_file', type=str, help='Path to file containing beat data, default dehuman_beat_data.asm', default='dehuman_beat_data.asm')
+    parser.add_argument('--note_file', type=str, help='Path to file containing note data, default dehuman_note_data.asm', default='dehuman_note_data.asm')
     parser.add_argument('--template', type=str, help='Path to template asm file, default dehuman_template.asm', default='dehuman_template.asm')
     parser.add_argument('--out_file', type=str, help='Path to write output file, default dehuman_generated_<timestamp>.asm', default=None)
 
@@ -66,8 +98,30 @@ if __name__ == '__main__':
     if args.out_file is None:
         args.out_file = 'dehuman_generated_{0}.asm'.format(int(time.time()))
 
-    print('Generating ASM using images {0} and {1} with template {2}'.format(args.foreground_image[0],
-                                                                             args.background_image[0],
-                                                                             args.template))
-    generate_asm(args.foreground_image[0], args.background_image[0], args.beat_file, args.note_file, args.out_file, args.template)
+    if args.foreground_image is None:
+        with open(args.fg_file) as f:
+            print('Using pre-constructed ASM in file {0} for foreground image'.format(args.fg_file))
+            fg_asm = f.read()
+    else:
+        print('Using image file {0} for foreground image'.format(args.foreground_image))
+        fg_asm = image_converter.gen_asm_from_image_file(args.foreground_image)
+
+    if args.background_image is None:
+        with open(args.bg_file) as f:
+            print('Using pre-constructed ASM in file {0} for background image'.format(args.bg_file))
+            bg_asm = f.read()
+    else:
+        print('Using image file {0} for background image'.format(args.background_image))
+        bg_asm = image_converter.gen_asm_from_image_file(args.backround_image)
+
+    print('Using beat data from {0} and note data from {1}'.format(args.beat_file, args.note_file))
+
+    with open(args.beat_file) as f:
+        beat_data = f.read()
+    with open(args.note_file) as f:
+        note_data = f.read()
+
+    print('Generating ASM using template {0}'.format(args.template))
+    generate_asm(fg_asm, bg_asm, beat_data, note_data, args.out_file, args.template)
+
     print('Generated ASM written to {0}'.format(args.out_file))
